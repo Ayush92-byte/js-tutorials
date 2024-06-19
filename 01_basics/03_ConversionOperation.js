8650705e31631f51f6cc1eddfbbf7b870e975dfd
AYUSH KUSHWAHA <100941282+Ayush92-byte@users.noreply.github.com>
2024-06-19 11:02:09 +0000

    3rd file added

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,44 @@
+// let score = "33"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+// // "33" => 33
+// // "33abc" => NaN
+// // true => 1; false => 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// "" => false
+// "string" => true
+
+/*               operations               */
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+
+let str1 = "hello"
+let str2 = " ayush"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// it will treat on the basis of which datatype comes first
+
+console.log();
+
+
+
+
