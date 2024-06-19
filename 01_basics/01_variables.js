ddc93110c311a72b5e14c8a212dec73baececd55
AYUSH KUSHWAHA <100941282+Ayush92-byte@users.noreply.github.com>
2024-06-19 06:38:02 +0000

    2 files added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144552
+let accountEmail = "ayush@gmail.com"
+var accountPassword = "12345"
+accountCity = "Haridwar"
+
+// accountId = 2 // cant do like that 
+
+/*
+Prefer not to use var 
+because of issue in block space and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
