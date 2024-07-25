366c1b593bb200fcef8e40fa5342c3b767d355cb
AYUSH KUSHWAHA <100941282+Ayush92-byte@users.noreply.github.com>
2024-07-25 15:23:30 +0000

    control flow files

---
+++ 04_control_flow/one.js
@@ -0,0 +1,19 @@
+// if
+
+const isUserloggedIn = true
+
+if(2 === "2") {
+    // console.log("executed");
+}
+
+// <, >, <=, >=, ==, !=, ===(checks type also), !== 
+
+// const score = 200
+
+// if(score > 100){
+//     const power = "fly"
+//     console.log(`User Power: ${power}`);
+// }
+
+// console.log(`User Power: ${power}`);
+
